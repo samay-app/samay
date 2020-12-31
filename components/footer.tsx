cd950f6eea6ae468f33cf5a6e98e3befb59a331b
VipinVIP <kpvipin33@gmail.com>
2020-12-31 09:25:20 +0530

    Fixed footer being sticky

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = (): JSX.Element => {
   return (
-    <footer className="py-3">
+    <footer className="py-3" style={{ position: "relative" }}>
       <div className="container text-center">
         <span className="font-weight-bold">Footer</span>
       </div>
