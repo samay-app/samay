59227378dec9ecaf2b8fee0c2089daa1c7cb9832
anandbaburajan <anandbaburajan@gmail.com>
2021-01-01 13:12:33 +0530

    no final choice yet in example

--- pages/poll/haha.tsx
+++ pages/poll/haha.tsx
@@ -20,7 +20,7 @@ const Poll = (): JSX.Element => {
     _id: "5fecb40047984b4c55764b5e",
     name: "testPoll",
     description: "testPollDescription",
-    open: false,
+    open: true,
     userID: "starman",
     interval: 3600000,
     choices: [
@@ -49,7 +49,6 @@ const Poll = (): JSX.Element => {
         choices: [2200268206000],
       },
     ],
-    finalChoice: 1609855679000,
     createdAt: "2020-12-30T17:08:16.765Z",
     updatedAt: "2020-12-30T17:08:16.765Z",
     __v: 0,
