f189dae6d3de0980eda6b15a83972b744001d04e
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-01-05 20:50:43 +0530

    Poll and mark final choice

--- src/components/SubmitChoices.tsx
+++ src/components/SubmitChoices.tsx
@@ -1,22 +1,58 @@
 import { Button } from "react-bootstrap";
 import { Vote } from "../models/poll";
+import Router from 'next/router'
 
-const SubmitChoices = (props: { newVote: Vote }): JSX.Element => {
-  const { newVote } = props;
+const SubmitChoices = (props: { newVote: Vote, pollid: string }): JSX.Element => {
+  const { newVote, pollid } = props;
 
   const handleSubmit = (): void => {
     // PUT newVote at v1/poll/:pollID
-  };
+    console.log(newVote);
+    console.log(pollid)
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    }
+    // TODO : Add authorisation header 
+    // headers["Authorization"] = "Bearer " + getIdToken();
+    const payload = JSON.stringify(newVote);
+
+    const requestOptions = {
+      method: "PUT",
+      headers: headers,
+      body: payload
+    };
+
+    console.log(payload)
+
+    fetch("http://localhost:5000/v1/poll/" + pollid, requestOptions)
+      .then((res) => {
+        if (res.status === 201) {
+          res.json()
+            .then((data) => {
+              console.log(data)
+              alert("Success!")
+              Router.reload(window.location.pathname);
+            })
+        } else {
+          console.log(res.status)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
 
   return (
-    <Button
-      variant="primary"
-      type="submit"
-      disabled={!newVote.userID || newVote.choices.length === 0}
-      onClick={handleSubmit}
-    >
-      Mark your choice
+    <div>
+      <Button
+        variant="primary"
+        type="submit"
+        disabled={!newVote.userID || newVote.choices.length === 0}
+        onClick={handleSubmit}
+      >
+        Mark your choice
     </Button>
+    </div>
   );
 };
 
--- src/components/SubmitFinalChoice.tsx
+++ src/components/SubmitFinalChoice.tsx
@@ -2,17 +2,48 @@ import { Button } from "react-bootstrap";
 import { Choice } from "../models/poll";
 
 const SubmitFinalChoice = (props: {
-  finalChoice: Choice | undefined;
+  finalChoice: Choice | undefined,
+  pollid: string
 }): JSX.Element => {
-  const { finalChoice } = props;
+  const { finalChoice, pollid } = props;
 
   const handleSubmit = (): void => {
     const markFinalChoice = {
-      finalChoice,
+      choices: finalChoice,
       open: false,
     };
     // PUT markFinalChoice at v1/user/poll/:pollid
-  };
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    }
+    // TODO : Add authorisation header 
+    // headers["Authorization"] = "Bearer " + getIdToken();
+    const payload = JSON.stringify(markFinalChoice);
+
+    const requestOptions = {
+      method: "PUT",
+      headers: headers,
+      body: payload
+    };
+
+    console.log(payload)
+    fetch("http://localhost:5000/v1/user/poll/" + pollid, requestOptions)
+      .then((res) => {
+        if (res.status === 201) {
+          res.json()
+            .then((data) => {
+              console.log(data)
+            })
+        } else {
+          console.log(res.status)
+          console.log(res)
+          Router.reload(window.location.pathname);
+        }
+      })
+      .catch((err) => console.log(err))
+  }
 
   return (
     <Button
