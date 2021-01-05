4dbbe5c91d3e897a54538eccda0e94db1e1df84e
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-01-05 20:49:24 +0530

    Show displayname in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,20 @@
 import Greetings from "../src/components/greeting";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Layout from "../src/components/layout";
-
 import withprivateAuth from "../src/utils/privateAuth"
+import { useSelector } from "react-redux";
 
 const Dashboard = (): JSX.Element => {
+
+  const displayName = useSelector((state) => state.authReducer.displayName);
+
   return (
     <Layout>
       <Container fluid>
         <Row>
           <Col>
             <h1>Dashboard</h1>
+            <h3> Welcome {displayName},</h3>
           </Col>
         </Row>
         <Row>
