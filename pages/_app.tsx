0e58785945e46b73c56f4f04f59d0355634ca20f
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-01-05 20:49:09 +0530

    Redux persist _app.ts setup change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,18 @@ import "../src/styles/global.css";
 import "../src/styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AppProps } from "next/app";
+import { wrapper } from "../src/store/store";
+import { useStore } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  const store = useStore((state) => state);
+
+  return (
+    <PersistGate persistor={store.__persistor} loading={<div>Loading</div>}>
+      <Component {...pageProps} />
+    </PersistGate>
+  );
 };
 
-export default App;
+export default wrapper.withRedux(App);
