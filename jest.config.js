7d4ebd0abfb535626849d4d28e27e5315af309f1
athul <athuljoshi7@gmail.com>
2021-01-23 22:01:36 +0530

    removed tests

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-      '@exmpl/(.*)': '<rootDir>/src/$1',
-    },
-  };
--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const environment: string = process.env.NODE_ENV || 'production';
 export const corsUrl: string = process.env.CORS_URL || '';
-export const port: number = parseInt(<string>process.env.PORT, 10) || 8080;
+export const port: number = parseInt(<string>process.env.PORT, 10) || 5000;
 export const loggerLevel: string = process.env.LOGGER_LEVEL || 'info';
 
 export const email: string = process.env.EMAIL || '';
