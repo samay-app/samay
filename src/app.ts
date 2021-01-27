84e5c681dfb1846fab785abd89f84bb56acac4d1
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-27 20:00:10 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50, // limit each IP to 50 requests per windowMs
+    max: 100, // limit each IP to 100 requests per windowMs
 });
 
 app.use(helmet());
