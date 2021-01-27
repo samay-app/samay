2483beac1967b4f27fee9dd5b9b2f39c7c821cef
athul <athuljoshi7@gmail.com>
2021-01-27 12:28:26 +0530

    helmet middleware added

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const environment: string = process.env.NODE_ENV || 'production';
-export const corsUrl: string = process.env.CORS_URL || '';
+export const corsURL: string = process.env.CORS_URL || '';
 export const port: number = parseInt(<string>process.env.PORT, 10) || 5000;
 export const loggerLevel: string = process.env.LOGGER_LEVEL || 'info';
 
