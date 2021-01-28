177eb409401473f77bab80d4d4b3ed96c8276a14
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-28 21:43:27 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
-app.options('*', cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
+app.options('*', cors());
 
 app.use(limiter);
 
