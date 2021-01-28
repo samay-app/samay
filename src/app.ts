5813baf6e6edfd30739a90433fe77698dd35c134
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-29 00:11:55 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,36 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import expressPino, { HttpLogger } from 'express-pino-logger';
+import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import pino, { Logger } from 'pino';
-import { logLevel } from './config';
+import { corsURL, logLevel } from './config';
 import './db/index'; // initialize database
 import routesV1 from './routes/v1/index';
 
 const logger: Logger = pino({ level: logLevel });
 const expressLogger: HttpLogger = expressPino({ logger });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
+app.options('*', cors());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(expressLogger);
 }
 
-app.use(cors({ origin: ["https://rocketmeet.me", "https://rocketmeet.me/dashboard"], credentials: true, optionsSuccessStatus: 200 }));
-app.options('*', cors());
-
 app.use(helmet());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.use('/v1', routesV1);
 
