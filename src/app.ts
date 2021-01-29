4595ad5682c347e2a6cc8da70b02bcd29ee755eb
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-29 19:47:42 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,25 @@
+import cors from 'cors';
 import express, { Application } from 'express';
 import expressPino, { HttpLogger } from 'express-pino-logger';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import pino, { Logger } from 'pino';
-import { logLevel } from './config';
+import { corsURL, logLevel } from './config';
 import './db/index'; // initialize database
 import routesV1 from './routes/v1/index';
 
 const logger: Logger = pino({ level: logLevel });
 const expressLogger: HttpLogger = expressPino({ logger });
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
 const app: Application = express();
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://rocketmeet.me');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin,Access,Content-Type,Authorization,X-Requested-With,X-Custom-Header');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if ('OPTIONS' === req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-});
+app.use(helmet());
+app.use(cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,6 +27,9 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') {
     app.use(expressLogger);
 }
+
+
+app.use(limiter);
 
 app.use('/v1', routesV1);
 
