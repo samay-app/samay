eae3eac7e1789e64c79b37e7627ecc3dad7eb321
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-29 22:03:16 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import expressPino, { HttpLogger } from 'express-pino-logger';
+import helmet from 'helmet';
 import pino, { Logger } from 'pino';
 import { corsURL, logLevel } from './config';
 import './db/index'; // initialize database
@@ -10,6 +11,8 @@ const logger: Logger = pino({ level: logLevel });
 const expressLogger: HttpLogger = expressPino({ logger });
 
 const app: Application = express();
+
+app.use(helmet());
 
 app.use(cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
 
