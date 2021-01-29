1d44c45aadd31b1c146bf027662ddf8c03f3d288
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-29 19:05:02 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import cors from 'cors';
 import express, { Application } from 'express';
 import expressPino, { HttpLogger } from 'express-pino-logger';
 import pino, { Logger } from 'pino';
-import { corsURL, logLevel } from './config';
+import { logLevel } from './config';
 import './db/index'; // initialize database
 import routesV1 from './routes/v1/index';
 
@@ -11,7 +10,18 @@ const expressLogger: HttpLogger = expressPino({ logger });
 
 const app: Application = express();
 
-app.use(cors({ origin: corsURL, credentials: true, optionsSuccessStatus: 200 }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://rocketmeet.me/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-Custom-Header');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if ('OPTIONS' === req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
