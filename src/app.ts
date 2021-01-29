bfc72b058529ef85b8d4f509290ee52f32bf928a
Anand Baburajan <anandbaburajan@gmail.com>
2021-01-29 18:39:16 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import expressPino, { HttpLogger } from 'express-pino-logger';
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
 import pino, { Logger } from 'pino';
 import { corsURL, logLevel } from './config';
 import './db/index'; // initialize database
@@ -10,11 +8,6 @@ import routesV1 from './routes/v1/index';
 
 const logger: Logger = pino({ level: logLevel });
 const expressLogger: HttpLogger = expressPino({ logger });
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-});
 
 const app: Application = express();
 
@@ -26,10 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') {
     app.use(expressLogger);
 }
-
-app.use(helmet());
-
-app.use(limiter);
 
 app.use('/v1', routesV1);
 
