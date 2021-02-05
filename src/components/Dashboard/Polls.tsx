bff3b95c49d6bbea33edcecea1979461d5d5ee70
VipinVIP <kpvipin33@gmail.com>
2021-02-05 20:23:04 +0530

    Fixed anomaly in pollsList

--- src/components/Dashboard/Polls.tsx
+++ src/components/Dashboard/Polls.tsx
@@ -13,60 +13,63 @@ const Polls: Function = (props: {
   setModalShow: Dispatch<boolean>;
 }): JSX.Element[] => {
   const { pollList, setPollIDToDelete, setModalShow } = props;
-  return pollList
-    .map((item: RocketMeetPollFromDB) => (
-      <div key={item._id}>
-        <Row>
-          <Col className="col-11">
-            <Card bg="dark" text="white" className="pt-4 px-4 my-2 cardindash">
-              <Row>
-                <div className="col-8">
-                  <Card.Title className="d-flex flex-row justify-content-between">
-                    <span className="card-title">{item.title}</span>
-                  </Card.Title>
-                </div>
-                <div className="col-4">
-                  <Badge
-                    pill
-                    variant={item.open ? "success" : "secondary"}
-                    className="rm-badge-dash"
-                  >
-                    {item.open ? "open" : "closed"}
-                  </Badge>
-                </div>
-              </Row>
-              <Card.Body className="text-justify">
-                <a
-                  href={`/poll/${item._id}`}
-                  aria-label="stretched link"
-                  className="stretched-link card-bdy "
+  const sorted = pollList.sort(
+    (a: RocketMeetPollFromDB, b: RocketMeetPollFromDB) => {
+      return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+    }
+  );
+  return sorted.map((item: RocketMeetPollFromDB) => (
+    <div key={item._id}>
+      <Row>
+        <Col className="col-11">
+          <Card bg="dark" text="white" className="pt-4 px-4 my-2 cardindash">
+            <Row>
+              <div className="col-8">
+                <Card.Title className="d-flex flex-row justify-content-between">
+                  <span className="card-title">{item.title}</span>
+                </Card.Title>
+              </div>
+              <div className="col-4">
+                <Badge
+                  pill
+                  variant={item.open ? "success" : "secondary"}
+                  className="rm-badge-dash"
                 >
-                  {item.description}
-                </a>
-              </Card.Body>
-              <Card.Footer className="px-0">
-                <span className="card-created">
-                  Created : {dayjs(item.createdAt).format("DD/MM/YYYY")}
-                </span>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col className="col-1 p-0">
-            <Button
-              variant="outline-light"
-              className="my-2 rm-delete-button"
-              onClick={(): void => {
-                setPollIDToDelete(item._id);
-                setModalShow(true);
-              }}
-            >
-              <Trash size="22" color="red " />
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    ))
-    .reverse();
+                  {item.open ? "open" : "closed"}
+                </Badge>
+              </div>
+            </Row>
+            <Card.Body className="text-justify">
+              <a
+                href={`/poll/${item._id}`}
+                aria-label="stretched link"
+                className="stretched-link card-bdy "
+              >
+                {item.description}
+              </a>
+            </Card.Body>
+            <Card.Footer className="px-0">
+              <span className="card-created">
+                Created : {dayjs(item.createdAt).format("DD/MM/YYYY")}
+              </span>
+            </Card.Footer>
+          </Card>
+        </Col>
+        <Col className="col-1 p-0">
+          <Button
+            variant="outline-light"
+            className="my-2 rm-delete-button"
+            onClick={(): void => {
+              setPollIDToDelete(item._id);
+              setModalShow(true);
+            }}
+          >
+            <Trash size="22" color="red " />
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  ));
 };
 
 export default Polls;
