140e290bb740d5788bec61466214d329e02e4df3
viveknathani <viveknathani2402@gmail.com>
2021-03-10 17:27:19 +0530

    Script to start all servers at once.

---
+++ dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A shell script to start the Node.js servers 
+# along with the mongod process, all of which
+# are necessary for RocketMeet-client to function.
+
+# Get the init system of the developer's platform.
+initSystem=`ps --no-headers -o comm 1`
+
+# Perform a check and start mongod.
+if [ "$initSystem" = "init" ]
+then
+    sudo service mongodb start
+else 
+    sudo systemctl start mongod
+fi
+
+# Start RocketMeet-server
+npm run dev --prefix ../RocketMeet-server/ &
+
+# Start RocketMeet-mailer
+npm run dev --prefix ../RocketMeet-mailer/ &
+
+# Start RocketMeet-client
+npm run dev
