420e026c67f670407f53c59f1ca4f1655fdb57c6
Anand <anandbaburajan@gmail.com>
2021-05-02 12:51:35 +0530

    remove pollcount

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,12 +1,10 @@
 import express, { Router } from 'express';
 import poll from './poll';
 import user from './user';
-import pollCount from './pollCount';
 
 const router: Router = express.Router();
 
 router.use('/poll', poll);
 router.use('/user', user);
-router.use('/pollCount', pollCount);
 
 export default router;
