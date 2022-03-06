80c377fd72f71dc1bae65d809b28704926b8123e
Anand <anandbaburajan@gmail.com>
2022-03-06 21:01:02 +0530

    Updating server and removing mailer

--- mailer/src/server.ts
+++
@@ -1,9 +0,0 @@
-import pino, { Logger } from 'pino';
-import { loggerLevel, port } from './config';
-import app from './app';
-
-const logger: Logger = pino({ level: loggerLevel });
-
-app.listen(port, () => {
-        logger.info(`Server running on port: ${port}`);
-    }).on('error', (err) => logger.error(err));
